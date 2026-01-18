399d11cde366873f1b5a59ba6c0498a8e408ad76
MizanRbf <mizanrbf@gmail.com>
2026-01-18 23:50:35 +0600

    problem created

--- Module-22/crud.ts
+++ Module-22/crud.ts
@@ -10,13 +10,35 @@ async function run() {
   // });
   // console.log("Created user", createUser);
   // Create post for user id = 1
-  const createPost = await prisma.post.create({
-    data: {
-      title: "this is title",
-      content: "this is content",
-      author_id: 1,
+  // const createPost = await prisma.post.create({
+  //   data: {
+  //     title: "this is title",
+  //     content: "this is content",
+  //     author_id: 1,
+  //   },
+  // });
+  // console.log("created post", createPost);
+
+  // Create Profile
+  // const createProfile = await prisma.profile.create({
+  //   data: {
+  //     bio: "hii",
+  //     user_id: 1,
+  //   },
+  // });
+  // console.log("profile created", createProfile);
+
+  // Find users
+  const users = await prisma.user.findMany({
+    // include: {
+    //   posts: true,
+    //   profile: true,
+    // },
+    select: {
+      posts: true,
+      profile: true,
     },
   });
-  console.log("created post", createPost);
+  console.dir(users, { depth: Infinity });
 }
 run();
